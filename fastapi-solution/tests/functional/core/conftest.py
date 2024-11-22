cc8a717d25a0603a6bc4bf1e36d293adfc7e8cbf
Vitaly D <vitcode.mail@gmail.com>
2024-11-22 13:04:25 +0300

    conftest doc strings added

--- fastapi-solution/tests/functional/core/conftest.py
+++ fastapi-solution/tests/functional/core/conftest.py
@@ -11,11 +11,10 @@ from redis.asyncio import Redis
 from core.settings import es_url, log, redis_url, service_url, test_settings
 from testdata.es_mapping import MOVIES_MAPPING
 
-CACHE_EXPIRE_IN_SECONDS = 60
-
 
 @pytest_asyncio.fixture(scope="session")
 def event_loop() -> Generator[asyncio.AbstractEventLoop, None, None]:
+    """Async loop fixture"""
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
@@ -23,6 +22,7 @@ def event_loop() -> Generator[asyncio.AbstractEventLoop, None, None]:
 
 @pytest_asyncio.fixture(name="es_client", scope="session")
 async def es_client() -> AsyncGenerator:
+    """Elasticsearch connection fixture"""
     es_client = AsyncElasticsearch(hosts=[es_url], verify_certs=False)
     yield es_client
     await es_client.close()
@@ -30,6 +30,7 @@ async def es_client() -> AsyncGenerator:
 
 @pytest_asyncio.fixture(name="redis_client", scope="session")
 async def redis_client() -> AsyncGenerator:
+    """Redis connection fixture"""
     redis_client = Redis(
         host=test_settings.redis_host, port=test_settings.redis_port
     )
@@ -39,12 +40,14 @@ async def redis_client() -> AsyncGenerator:
 
 @pytest_asyncio.fixture(name="aiohttp_session", scope="session")
 async def aiohttp_session() -> AsyncGenerator:
+    """API connection fixture"""
     async with aiohttp.ClientSession() as session:
         yield session
 
 
 @pytest_asyncio.fixture(name="es_write_data")
 def es_write_data(es_client: AsyncElasticsearch) -> Callable:
+    """Elasticsearch write data fixture"""
 
     async def inner(index_: str, data: list[dict]) -> None:
         log.debug("\nes_url: \n%s\n", es_url)
@@ -61,27 +64,9 @@ def es_write_data(es_client: AsyncElasticsearch) -> Callable:
     return inner
 
 
-@pytest_asyncio.fixture(name="redis_get_data")
-def redis_get_data(redis_client: Redis) -> Callable:
-
-    async def inner(key: str) -> None:
-        log.debug("\nredis_url: \n%s\n", redis_url)
-
-        try:
-            cached_data = await redis_client.get(key)
-
-        except:
-            cached_data = None
-            # Exception("Error reading data from Redis")
-
-        result = True if cached_data is not None else False
-        return result
-
-    return inner
-
-
 @pytest_asyncio.fixture(name="es_check_data")
 def es_check_data(es_client: AsyncElasticsearch) -> Callable:
+    """Elasticsearch check data fixture"""
 
     async def inner(index_: str, event: asyncio.Event, quantity: int) -> None:
 
@@ -105,8 +90,29 @@ def es_check_data(es_client: AsyncElasticsearch) -> Callable:
     return inner
 
 
+@pytest_asyncio.fixture(name="redis_get_data")
+def redis_get_data(redis_client: Redis) -> Callable:
+    """Redis get data fixture"""
+
+    async def inner(key: str) -> None:
+        log.debug("\nredis_url: \n%s\n", redis_url)
+
+        try:
+            cached_data = await redis_client.get(key)
+
+        except:
+            cached_data = None
+            # Exception("Error reading data from Redis")
+
+        result = True if cached_data is not None else False
+        return result
+
+    return inner
+
+
 @pytest_asyncio.fixture(name="make_get_request")
 def make_get_request(aiohttp_session) -> Callable:
+    """API request fixture"""
 
     async def inner(
         event: asyncio.Event, service_urn: str, query_data: dict = None
