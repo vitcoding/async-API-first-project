3afe7a66d6ce772a804bde1aaf20cd5ddb9ce6bb
Vitaly D <vitcode.mail@gmail.com>
2024-11-22 09:36:06 +0300

    Films_list tests added

--- fastapi-solution/tests/functional/src/test_films.py
+++ fastapi-solution/tests/functional/src/test_films.py
@@ -58,57 +58,59 @@ async def test_films(es_write_data, es_check_data, make_get_request) -> None:
     assert status == 404
 
 
-# @pytest.mark.parametrize(
-#     "query_data, expected_answer",
-#     [
-#         ({}, {"status": 200, "length": 50}),
-#         ({"sort": "imdb_rating"}, {"status": 200, "length": 50}),
-#         # ({"page_size": 30}, {"status": 200, "length": 30}),
-#         # ({"page_size": 0}, {"status": 422, "length": 1}),
-#         # ({"page_number": 2}, {"status": 200, "length": 50}),
-#         # ({"page_number": 3}, {"status": 200, "length": 20}),
-#         # ({"page_number": 4}, {"status": 404, "length": 1}),
-#         # (
-#         #     {"genre": "6c162475-c7ed-4461-9184-001ef3d9f26e"},
-#         #     {"status": 200, "length": 50},
-#         # ),
-#         # ########## error
-#         # (
-#         #     {"genre": "Drama"},
-#         #     {"status": 200, "length": 2},
-#         # ),
-#         # ##########
-#         # (
-#         #     {
-#         #         "sort": "-imdb_rating",
-#         #         "page_size": 30,
-#         #         "page_number": 3,
-#         #         "genre": "6c162475-c7ed-4461-9184-001ef3d9f26e",
-#         #     },
-#         #     {"status": 200, "length": 30},
-#         # ),
-#     ],
-# )
-# @pytest.mark.asyncio(loop_scope="session")
-# async def test_films_list(
-#     es_write_data, es_check_data, make_get_request, query_data, expected_answer
-# ) -> None:
-#     event = asyncio.Event()
-#     quantity = 120
-#     await es_load_data(es_write_data, es_check_data, event, quantity)
+@pytest.mark.parametrize(
+    "query_data, expected_answer",
+    [
+        ({}, {"status": 200, "length": 50}),
+        ({"sort": "imdb_rating"}, {"status": 200, "length": 50}),
+        # ({"page_size": 30}, {"status": 200, "length": 30}),
+        # ({"page_size": 0}, {"status": 422, "length": 1}),
+        # ({"page_number": 2}, {"status": 200, "length": 50}),
+        # ({"page_number": 3}, {"status": 200, "length": 20}),
+        # ({"page_number": 4}, {"status": 404, "length": 1}),
+        # (
+        #     {"genre": "6c162475-c7ed-4461-9184-001ef3d9f26e"},
+        #     {"status": 200, "length": 50},
+        # ),
+        # (
+        #     {"genre": "6c162475-c7ed-4461-9184-001ef3d9none"},
+        #     {"status": 404, "length": 1},
+        # ),
+        # (
+        #     {"genre": "Action"},
+        #     {"status": 404, "length": 1},
+        # ),
+        # (
+        #     {
+        #         "sort": "-imdb_rating",
+        #         "page_size": 30,
+        #         "page_number": 3,
+        #         "genre": "6c162475-c7ed-4461-9184-001ef3d9f26e",
+        #     },
+        #     {"status": 200, "length": 30},
+        # ),
+    ],
+)
+@pytest.mark.asyncio(loop_scope="session")
+async def test_films_list(
+    es_write_data, es_check_data, make_get_request, query_data, expected_answer
+) -> None:
+    event = asyncio.Event()
+    quantity = 120
+    await es_load_data(es_write_data, es_check_data, event, quantity)
 
-#     search_urn = "/api/v1/films/"
-#     status, _, body = await make_get_request(event, search_urn, query_data)
+    search_urn = "/api/v1/films/"
+    status, _, body = await make_get_request(event, search_urn, query_data)
 
-#     assert status == expected_answer["status"]
-#     assert len(body) == expected_answer["length"]
+    assert status == expected_answer["status"]
+    assert len(body) == expected_answer["length"]
 
 
 @pytest.mark.parametrize(
     "query_data, expected_answer",
     [
-        # ({"query": "The Star"}, {"status": 200, "length": 50}),
-        # ({"query": "Mashed potato"}, {"status": 404, "length": 1}),
+        ({"query": "The Star"}, {"status": 200, "length": 50}),
+        ({"query": "Mashed potato"}, {"status": 404, "length": 1}),
         # (
         #     {"query": "The Star", "page_size": 50, "page_number": 0},
         #     {"status": 404, "length": 1},
